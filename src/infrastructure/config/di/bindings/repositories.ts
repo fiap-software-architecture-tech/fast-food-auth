61135955ed878de9a59328924d089a851e21ab8b
Fabio Smaniotto Bernardes <fabio.bernardes.gft@naturapay.net>
2026-01-06 19:20:38 -0300

    fix: remove unnecessary bindings

--- src/infrastructure/config/di/bindings/repositories.ts
+++
@@ -1,9 +0,0 @@
-import { Container } from 'inversify';
-
-import { IClientRepository } from '#/domain/repositories/client.repository';
-import { TYPES } from '#/infrastructure/config/di/types';
-import { PrismaClientRepository } from '#/infrastructure/repositories/prisma/prisma-client.repository';
-
-export function bindRepositories(container: Container) {
-    container.bind<IClientRepository>(TYPES.ClientRepository).to(PrismaClientRepository).inSingletonScope();
-}
